2d9e90e70a63780a59b45ad2fcf416f78373027a
thiagoribb <thiribeiro142@gmail.com>
2021-09-27 16:16:48 -0300

    Preparando revisão de código

--- server/src/schemas/signUp.ts
+++
@@ -1,7 +0,0 @@
-import joi from "joi";
-
-export default joi.object({
-  email: joi.string().email().required(),
-  password: joi.string().required(),
-  confirmPassword: joi.string().required(),
-});
